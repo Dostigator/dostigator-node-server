f40cf86c20fed641814ba1eb7e01a3c6de6c9b12
bakalux <swaveful@gmail.com>
2018-10-04 01:54:45 +0300

    added put, post, delete

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const data = require("./app/data");
 
-const someJSON = {
-  hello: "heh, darova",
-  name: "Kolyan"
-};
+app.get("/achievements", (request, response) => {
+  response.send(data);
+});
 
-app.get("/test", (request, response) => {
-  response.send(someJSON);
+app.get("/achievements/:id", (request, response) => {
+  response.send(data[request.params.id]);
+});
+
+app.post("/achievements", (request, response) => {
+  console.log(request.body);
+  const achievement = request.body;
+  data.push({
+    name: achievement.name,
+    id: data.length
+  });
+  response
+    .status(201)
+    .send(`acheivement with body ${achievement} posted successfully!`);
+});
+
+app.delete("/achievements/:id", (request, response) => {
+  const achievement = data[request.params.id];
+  delete data[request.params.id];
+  response
+    .status(201)
+    .send(`achievement with body ${achievement} deleted successsfully`);
+});
+
+app.put("/achievements/:id", (request, response) => {
+  const achievement = req.body;
+  data[request.params.id].name = achievement.name;
+  response
+    .status(204)
+    .send(`achievement with body ${achievement} put successsfully`);
 });
 
 app.listen(port, err => {
